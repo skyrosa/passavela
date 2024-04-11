d4f321eb8e174c259225392d67d15dcfeb7873c0
Scarlet Rosa <likedyourbody@gmail.com>
2024-04-11 19:10:33 +0200

    no funciona

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,22 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
 export default function AdminDashboard() {
-    return <h1>Dashboard</h1>
+    return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Card>
+            <CardHeader>
+                <CardTitle>Sales</CardTitle>
+                </CardHeader>
+            <CardDescription>Desc</CardDescription>
+            <CardContent>
+                <p>Text</p>
+            </CardContent>
+        </Card>
+    </div>
 }
+
